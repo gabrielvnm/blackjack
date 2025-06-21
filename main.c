d41af371536c555136f937e3ec1e31016e97672a
gabrielvnm <gabrielvnm@gmail.com>
2025-06-21 17:05:47 -0300

    update switch

--- main.c
+++ main.c
@@ -7,44 +7,67 @@
 
 int main(){
     
-    int op;
+    int op = 0;
     char nome_jogador[MAX_NOME_JOGADOR];
-/*
+
     printf("Bem vindo ao jogo de BlackJack!\n");
-    printf("Selecione uma opção:\n");
-    printf("1: Novo Jogo\n2: Ver histórico de pontuação\n3: Regras\n 4: Sair\n");
-    scanf("%d",&op);
-    
-    switch (op)
-    {
-        default:
-            printf("Erro! Digite uma opcao valida!\n");
-            break;
-        case 1:*/
-            printf("Digite o nome do jogador:\n");
-            fgets(nome_jogador, MAX_NOME_JOGADOR, stdin);
-            nome_jogador[strcspn(nome_jogador, "\n")] = '\0';
-            printf("nome do jogador: %s\n",nome_jogador);
+    printf("Digite o nome do jogador:\n");
+    fgets(nome_jogador, MAX_NOME_JOGADOR, stdin);
+    nome_jogador[strcspn(nome_jogador, "\n")] = '\0';
+    printf("nome do jogador: %s\n",nome_jogador);
 
-            Jogador jogador1 = criarJogador(nome_jogador);
-            Jogador dealer = criarJogador("Dealer");
-            Carta* baralho = inicializarBaralho();
-            embaralharBaralho(&baralho);
-            turnoDoDealer(&dealer, &baralho);
-            turnoDoJogador(&jogador1, &baralho);
-
-/*
-            break;
-        case 2:
-
-            break;
-        case 3:
-
-            break;
-        case 4:
-
-            break;
+    while (op!=4){
         
-    }*/
+        printf("Selecione uma opção:\n");
+        printf("1: Novo Jogo\n2: Ver histórico de pontuação\n3: Regras\n4: Sair\n");
+        scanf("%d",&op);
+        switch (op){
+            default:
+                printf("Erro! Digite uma opcao valida!\n");
+                break;
+            case 1:
+                Jogador jogador1 = criarJogador(nome_jogador);
+                Jogador dealer = criarJogador("Dealer");
+                Carta* baralho = inicializarBaralho();
+                embaralharBaralho(&baralho);
+                turnoDoDealer(&dealer, &baralho);
+                turnoDoJogador(&jogador1, &baralho);
+                if (jogador1.pontuacao >21){
+                    printf("voce perdeu!\n");
+                    salvarPontuacao(nome_jogador,jogador1.pontuacao);
+                    salvarPontuacao(dealer.nome,dealer.pontuacao);
+                }
+                else if(jogador1.pontuacao == 21){
+                    printf("voce venceu!\n");
+                    salvarPontuacao(nome_jogador,jogador1.pontuacao);
+                    salvarPontuacao(dealer.nome,dealer.pontuacao);
+                }
+                else if(jogador1.pontuacao<21 && jogador1.pontuacao>dealer.pontuacao){
+                    printf("voce venceu!\n");
+                    salvarPontuacao(nome_jogador,jogador1.pontuacao);
+                    salvarPontuacao(dealer.nome,dealer.pontuacao);
+                }
+                else if(jogador1.pontuacao<21 && jogador1.pontuacao<dealer.pontuacao){
+                    printf("voce perdeu!\n");
+                    salvarPontuacao(nome_jogador,jogador1.pontuacao);
+                    salvarPontuacao(dealer.nome,dealer.pontuacao);
+                }
+                else if(jogador1.pontuacao<21 && dealer.pontuacao>21){
+                    printf("voce venceu!\n");
+                    salvarPontuacao(nome_jogador,jogador1.pontuacao);
+                    salvarPontuacao(dealer.nome,dealer.pontuacao);
+                }
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+                break;
+            
+        }
+    }
     return 0;
 }
